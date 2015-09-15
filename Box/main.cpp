76e9b923796dac5ffefbf7fa812cc9f77a787361
Matthew Dillard <7395051@gmail.com>
2015-09-15 13:28:41 -0500

    Initial Commit

---
+++ Box/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Box
+//
+//  Created by Matthew Dillard on 9/15/15.
+//  Copyright (c) 2015 Matthew Dillard. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
